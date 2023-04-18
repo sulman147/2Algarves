69095393ebc6320367ec3448235d239856eb9cb8
Muhammad Sulman <msulman147@gmail.com>
2023-04-18 18:49:31 +0500

    add algave town in search option

--- app/hooks/useCountries.ts
+++ app/hooks/useCountries.ts
@@ -1,12 +1,18 @@
-import countries from 'world-countries';
+// import countries from 'world-countries';
+import countries from './algarve.json';
+
+
 
 const formattedCountries = countries.map((country) => ({
-  value: country.cca2,
-  label: country.name.common,
-  flag: country.flag,
+  
+  value: country.iso2,
+  label: country.city,
+  flag: country.iso2,
   latlng: country.latlng,
-  region: country.region,
+  region: country.country,
 }));
+console.log("this is my formated countries",countries)
+console.log("this is my formated countries",formattedCountries)
 
 const useCountries = () => {
   const getAll = () => formattedCountries;
