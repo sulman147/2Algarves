7d2e491e2475dde1a790bb2878cc61dafbecf405
Muhammad Sulman <Sulman.Siddique@mavenx.gg>
2023-05-20 16:42:10 +0500

    no message

--- app/reservations/ReservationsClient.tsx
+++ app/reservations/ReservationsClient.tsx
@@ -8,7 +8,7 @@ import { useRouter } from "next/navigation";
 import { SafeReservation, SafeUser } from "@/app/types";
 import Heading from "@/app/components/Heading";
 import Container from "@/app/components/Container";
-import ReservationCard from "@/app/components/listings/ReservationCard";
+import ListingCard from "../components/listings/ListingCard";
 
 interface ReservationsClientProps {
   reservations: SafeReservation[],
@@ -45,27 +45,27 @@ const ReservationsClient: React.FC<ReservationsClientProps> = ({
         subtitle="Bookings on your properties"
       />
      <div 
-          className="
-            pt-24
-            grid 
-            grid-cols-1 
-            sm:grid-cols-2 
-            md:grid-cols-3 
-            lg:grid-cols-3
-            xl:grid-cols-4
-            2xl:grid-cols-4
-            gap-8
-          "
-        >
+        className="
+          mt-10
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
         {reservations.map((reservation: any) => (
-          <ReservationCard
+          <ListingCard
             key={reservation.id}
             data={reservation.listing}
             reservation={reservation}
             actionId={reservation.id}
             onAction={onCancel}
             disabled={deletingId === reservation.id}
-            actionLabel="Cancel guest reservation"
+            actionLabel="Cancel reservation"
             currentUser={currentUser}
           />
         ))}
--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -9,7 +9,7 @@ import { SafeReservation, SafeUser } from "@/app/types";
 
 import Heading from "@/app/components/Heading";
 import Container from "@/app/components/Container";
-import ListingCard from "@/app/components/listings/ListingCard";
+import ReservationCard from "../components/listings/ReservationCard";
 
 interface TripsClientProps {
   reservations: SafeReservation[],
@@ -47,26 +47,26 @@ const TripsClient: React.FC<TripsClientProps> = ({
       />
       <div 
         className="
-          mt-10
-          grid 
-          grid-cols-1 
-          sm:grid-cols-2 
-          md:grid-cols-3 
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8
-        "
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-3
+            xl:grid-cols-4
+            2xl:grid-cols-4
+            gap-8
+          "
       >
         {reservations.map((reservation: any) => (
-          <ListingCard
+          <ReservationCard
             key={reservation.id}
             data={reservation.listing}
             reservation={reservation}
             actionId={reservation.id}
             onAction={onCancel}
             disabled={deletingId === reservation.id}
-            actionLabel="Cancel reservation"
+            actionLabel="Cancel guest reservation"
             currentUser={currentUser}
           />
         ))}
