7e5938e2d4b305f3cd37f2f692ede3257cd2d38f
mzubairhassan <mzubairhassan18@gmail.com>
2023-05-23 16:38:19 +0500

    fix hypens

--- app/about/AboutClient.tsx
+++ app/about/AboutClient.tsx
@@ -24,12 +24,12 @@ const AboutClient: React.FC = ({
       <span className="text-2xl font-bold underline mb-5">About</span>
       <div className="text-left w-4/6">
 
-        <h2 className="text-4xl font-bold mb-5 underline decoration-[#58a4ab]">We help people bring their creative ideas to life</h2>
+        <h2 className="text-4xl font-bold mb-5 ">We help people bring their creative ideas to life</h2>
         <p className="mb-5">Millions of people around the world visit 2algarve to buy and sell creative assets, use
           smart design templates, learn creative skills or even hire freelancers. With an industryleading marketplace paired with an unlimited subscription service, 2algarve helps
           creatives like you get projects done faster</p>
-          <p>Whether it's graphic templates, website themes, photos, video or audio, there's every
-          chance you’ll spot something from 2algarve today, from a café logo to a Hollywood title
+          <p>Whether it&rsquo;s graphic templates, website themes, photos, video or audio, there&rsquo;s every
+          chance you&rsquo;ll spot something from 2algarve today, from a café logo to a Hollywood title
           sequence!
           </p>
       </div>
@@ -45,7 +45,7 @@ const AboutClient: React.FC = ({
         width={300} height={200}  alt={""}
         
       /></div>
-          <div>John Thompson is the visionary founder of XYZ Company, a transformative leader with a passion for innovation and exceptional business acumen. With a diverse background and strategic planning skills, he has propelled XYZ Company to the forefront of its field. John's relentless pursuit of excellence, ability to identify emerging trends, and foster a culture of creativity have driven the company's remarkable success. He inspires and empowers his team, invests in people, and promotes social responsibility. Under his guidance, XYZ Company continues to revolutionize industries, making John a trailblazing founder and industry luminary.</div>
+          <div>John Thompson is the visionary founder of XYZ Company, a transformative leader with a passion for innovation and exceptional business acumen. With a diverse background and strategic planning skills, he has propelled XYZ Company to the forefront of its field. John&rsquo;s relentless pursuit of excellence, ability to identify emerging trends, and foster a culture of creativity have driven the company&rsquo;s remarkable success. He inspires and empowers his team, invests in people, and promotes social responsibility. Under his guidance, XYZ Company continues to revolutionize industries, making John a trailblazing founder and industry luminary.</div>
         </div>
       </div>
     </div>
