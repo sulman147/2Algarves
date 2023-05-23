77b816c57f9472028c1525e8920b7866b2507bb5
Muhammad Sulman <Sulman.Siddique@mavenx.gg>
2023-05-23 16:27:05 +0500

    Set About us changes for build error

--- app/about/AboutClient.tsx
+++ app/about/AboutClient.tsx
@@ -28,9 +28,9 @@ const AboutClient: React.FC = ({
         <p className="mb-5">Millions of people around the world visit 2algarve to buy and sell creative assets, use
           smart design templates, learn creative skills or even hire freelancers. With an industryleading marketplace paired with an unlimited subscription service, 2algarve helps
           creatives like you get projects done faster</p>
-          <p>Whether it's graphic templates, website themes, photos, video or audio, there's every
+          <p>{`Whether it's graphic templates, website themes, photos, video or audio, there's every
           chance you’ll spot something from 2algarve today, from a café logo to a Hollywood title
-          sequence!
+          sequence!`}
           </p>
       </div>
       <div className="w-4/6 text-center mt-5 mb-5 p-10">
@@ -45,7 +45,7 @@ const AboutClient: React.FC = ({
         width={300} height={200}  alt={""}
         
       /></div>
-          <div>John Thompson is the visionary founder of XYZ Company, a transformative leader with a passion for innovation and exceptional business acumen. With a diverse background and strategic planning skills, he has propelled XYZ Company to the forefront of its field. John's relentless pursuit of excellence, ability to identify emerging trends, and foster a culture of creativity have driven the company's remarkable success. He inspires and empowers his team, invests in people, and promotes social responsibility. Under his guidance, XYZ Company continues to revolutionize industries, making John a trailblazing founder and industry luminary.</div>
+          <div>{`John Thompson is the visionary founder of XYZ Company, a transformative leader with a passion for innovation and exceptional business acumen. With a diverse background and strategic planning skills, he has propelled XYZ Company to the forefront of its field. John's relentless pursuit of excellence, ability to identify emerging trends, and foster a culture of creativity have driven the company's remarkable success. He inspires and empowers his team, invests in people, and promotes social responsibility. Under his guidance, XYZ Company continues to revolutionize industries, making John a trailblazing founder and industry luminary.`}</div>
         </div>
       </div>
     </div>
