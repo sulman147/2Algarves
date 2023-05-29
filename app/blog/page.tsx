d1f568844ff67369a7fb550b707a0d7e913005a5
Muhammad Sulman <Sulman.Siddique@mavenx.gg>
2023-05-29 20:18:26 +0500

    no message

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -27,8 +27,8 @@ const BlogPage = async () => {
           grid-cols-1 
           sm:grid-cols-2 
           md:grid-cols-3 
-          lg:grid-cols-3
-          xl:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-4
           2xl:grid-cols-4
           gap-8
         "
--- app/post/[postid]/PostClient.tsx
+++ app/post/[postid]/PostClient.tsx
@@ -1,30 +1,11 @@
 'use client';
 
-import axios from "axios";
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { toast } from "react-hot-toast";
-import { Range } from "react-date-range";
-import { useRouter } from "next/navigation";
-import { differenceInDays, eachDayOfInterval } from 'date-fns';
-
-import useLoginModal from "@/app/hooks/useLoginModal";
-import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
-
 import Container from "@/app/components/Container";
-import { categories } from "@/app/components/navbar/Categories";
-import ListingHead from "@/app/components/listings/ListingHead";
-import ListingInfo from "@/app/components/listings/ListingInfo";
-import ListingReservation from "@/app/components/listings/ListingReservation";
-import ContactInfo from "@/app/components/listings/ContactInfo";
-import Ammenities from "@/app/components/listings/Ammenities";
-import Gallery from "@/app/components/listings/Gallery";
-import Reviews from "@/app/components/listings/Reviews";
-import LocationCard from "@/app/components/listings/LocationCard";
 import Image from "next/image";
 import Input from "@/app/components/inputs/Input";
 import { FieldValues, useForm } from "react-hook-form";
 import Button from "@/app/components/Button";
-import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaFacebook, FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 interface Props {
   postId: number;
@@ -45,23 +26,78 @@ const PostClient: React.FC<Props> = ({ postId
   });
 
   return ( 
+    
     <Container>
-      <div 
-        className="
-          max-w-screen-lg 
-          mx-auto
-        "
-      > 
-      <div className="mb-[45px]"><h2 className="text-2xl font-bold">Another Article</h2></div>
-      <div className="grid grid-cols-5 gap-3">
-        <div className="col-span-4">
+      <div className=" h-[50vh] gap-5 flex flex-row w-full">
+        <div className="flex flex-col  w-[70%]">
+        <h2 className=" text-xl font-bold" >The Unexpected Benefits that Couchsurfing Brings</h2>
         <Image
-          src="/images/aboutpic.png"
+          src="/images/1.jpg"
             width={300} height={600}  alt={""} unoptimized={true}
-            className="w-full mb-5 -mt-10 "
+            className="w-full h-[50vh] mb-5 rounded-lg object-cover mt-2"
           />
-          <span className="bg-slate-400 mb-10 text-white p-3 rounded-md">23 Feb, 2023</span>
-          <div className="mt-5">
+        </div>
+        <div className="bg-white mt-8 h-full rounded-xl align-middle p-5 gap-5 flex flex-col w-[30%]">
+        <Input
+          id="name"
+          label="Name"
+          required register={register} errors={errors}      />
+        <Input
+          id="email"
+          label="Email"
+          required register={register} errors={errors}      />
+          <Button 
+          disabled={false}
+          label="Subscribe"
+          onClick={()=>{}}
+        />
+        <hr />
+        <div className="flex justify-center  gap-5">
+        <div 
+            className="
+            p-2 
+            bg-pink-750 
+            rounded-full 
+            text-white
+            hover:bg-orange-700 
+            hover:text-white 
+            "
+          >
+           <FaFacebookF size={20}/> </div>
+        <div 
+            className="
+            p-2 
+            bg-pink-750 
+            rounded-full 
+            text-white
+            hover:bg-orange-700 
+            hover:text-white 
+            "
+          >
+          <FaTwitter size={20} />  </div>
+          <div 
+            className="
+            p-2 
+            bg-pink-750 
+            rounded-full 
+            text-white
+            hover:bg-orange-700 
+            hover:text-white 
+            "
+          >
+          <FaInstagram size={20} /> </div>
+        </div>
+        
+        
+
+        </div>
+
+      </div>
+      <div className="bg-white rounded-xl  mt-16 flex flex-col p-6 w-full">
+      <div className="">
+      <span className="font-normal bg-[#cb1670] text-white px-2 py-2 rounded-md text-sm">25 May, 2023</span>
+        </div>
+        <div className="mt-3 text-lg font-light">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dicta accusamus est nostrum & 
             odit voluptate tempora&#44; officia at corrupti sit nihil molestiae ratione doloremque 
             similique quia&#44; ipsa culpa vitae sunt.
@@ -81,27 +117,6 @@ const PostClient: React.FC<Props> = ({ postId
             odit voluptate tempora&#44; officia at corrupti sit nihil molestiae ratione doloremque 
             similique quia&#44; ipsa culpa vitae sunt.
           </div>
-          </div>
-        <div className="col-auto flex flex-col gap-5">
-        <Input
-          id="name"
-          label="Name"
-          required register={register} errors={errors}      />
-        <Input
-          id="email"
-          label="Email"
-          required register={register} errors={errors}      />
-          <Button 
-          disabled={false}
-          label="Subscribe"
-          onClick={()=>{}}
-        />
-        <hr />
-        <h3>Social Links</h3>
-        <div className="flex gap-5"><FaFacebook size={40}/> <FaTwitter size={40} /> <FaInstagram size={40} /></div>
-        </div>
-
-      </div>
       </div>
         
     </Container>
